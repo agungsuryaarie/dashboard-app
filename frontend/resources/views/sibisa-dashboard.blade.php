e1479a529e8665add5a72828e1771b7678f7c3ed
abiihutabarat29 <abiihutabarat97@gmail.com>
2022-12-28 12:42:42 +0700

    add routes reload data

--- frontend/resources/views/sibisa-dashboard.blade.php
+++ frontend/resources/views/sibisa-dashboard.blade.php
@@ -9,7 +9,7 @@
                             <h2 class="text-lg font-medium truncate mr-5">
                                 Data Penduduk Kabupaten Batu Bara
                             </h2>
-                            <a href="" class="ml-auto flex items-center text-primary"> <svg
+                            <a href="{{ route('sibisa') }}" class="ml-auto flex items-center text-primary"> <svg
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                     stroke-linejoin="round" icon-name="refresh-ccw" data-lucide="refresh-ccw"
